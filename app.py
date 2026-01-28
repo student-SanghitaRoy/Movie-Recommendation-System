2fd3acb1a7ec77a4e9c0a7237120cfd5133a1ecb
Sanghita Roy <rupsaseharawork@gmail.com>
2026-01-28 20:23:01 +0530

    Update

--- app.py
+++ app.py
@@ -126,9 +126,11 @@ def get_recommendation():
 # Run App
 
 
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
